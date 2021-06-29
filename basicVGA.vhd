a9ac92dd23121e0da3cb3350e2ab307d308e4caf
LukasZrout <59703686+LukasZrout@users.noreply.github.com>
2021-06-29 10:44:03 +0200

    Update basicVGA.vhd

--- basicVGA.vhd
+++ basicVGA.vhd
@@ -30,191 +30,189 @@
 -- @date 5.12.2020
 --       May 12th 2020
 
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-
-
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
 --todo: failsave integer minus; reset; precalculate values (y<=600-120 to y<=480);
 
 --frage: wie reset bei register, letzter process
-entity basicVGA is
-    generic(
-        -- inital X and Y and X- and Y-Direction values of the object
-        -- base is the top left corner
-        -- direction 1, equals to positive increment, 0 equals to negative increment
-        initX:		integer:= 100; -- 0 to 267
-        initY:		integer:= 200; -- 0 to 600
-        initXdir:	BIT    := '1';
-        initYdir:	BIT    := '0';
+ENTITY basicVGA IS
+	GENERIC (
+		-- inital X and Y and X- and Y-Direction values of the object
+		-- base is the top left corner
+		-- direction 1, equals to positive increment, 0 equals to negative increment
+		initX : INTEGER := 100; -- 0 to 267
+		initY : INTEGER := 200; -- 0 to 600
+		initXdir : BIT := '1';
+		initYdir : BIT := '0';
 
-        obj:        BIT    := '1' -- 1 Circle; 0 Square;
-    );
-    port(
-        CLK     : in    std_logic;
-        HSYNC   : out   std_logic;
-        VSYNC   : out 	std_logic;
-        R       : out 	std_logic;
-        G       : out 	std_logic;
-        B       : out 	std_logic;
-		beep    : out 	std_logic;
-        nRES    : in 	std_logic
-    );
-end entity basicVGA;
-architecture basicVGA_arch of basicVGA is
-    signal HCOUNT : natural range 0 to 343 := 0;
-    signal VCOUNT : natural range 0 to 626 := 0;
-    signal x : 		natural range 0 to 266 :=initX;
-    signal y : 		natural range 0 to 600 :=initY;
-    signal xDir : 	BIT :=initXdir;
-    signal yDir : 	BIT :=initYdir;
-	signal frame : 	BIT := '0';
-	 
-	signal rgb, squareCol: std_logic_vector(2 downto 0):="000";
-	 --signal squareCol:std_logic_vector(2 downto 0):="000";
-	 
-begin
-	R<=rgb(0);
-	G<=rgb(1);
-    B<=rgb(2);
-    squareCol<= "100" when xDir&yDir = "00" else
-                "010" when xDir&yDir = "01" else
-                "001" when xDir&yDir = "10" else
-                "011" when xDir&yDir = "11";
+		obj : BIT := '1' -- 1 Circle; 0 Square;
+	);
+	PORT (
+		CLK : IN STD_LOGIC;
+		HSYNC : OUT STD_LOGIC;
+		VSYNC : OUT STD_LOGIC;
+		R : OUT STD_LOGIC;
+		G : OUT STD_LOGIC;
+		B : OUT STD_LOGIC;
+		beep : OUT STD_LOGIC;
+		nRES : IN STD_LOGIC
+	);
+END ENTITY basicVGA;
+ARCHITECTURE basicVGA_arch OF basicVGA IS
+	SIGNAL HCOUNT : NATURAL RANGE 0 TO 343 := 0;
+	SIGNAL VCOUNT : NATURAL RANGE 0 TO 626 := 0;
+	SIGNAL x : NATURAL RANGE 0 TO 266 := initX;
+	SIGNAL y : NATURAL RANGE 0 TO 600 := initY;
+	SIGNAL xDir : BIT := initXdir;
+	SIGNAL yDir : BIT := initYdir;
+	SIGNAL frame : BIT := '0';
 
-    count_p:process(CLK, nRES) --counts and automatically resets the X and Y pixels
-    begin
-        if(nRES='1') then
-				if rising_edge(CLK) then
-					if(HCOUNT/=341) then
-						 HCOUNT<=HCOUNT+1;
-					else 
-						HCOUNT<=0;
-						if(VCOUNT/=625) then
-							VCOUNT<=VCOUNT+1;
-						else
-							VCOUNT<=0;
-						end if;
-					end if;
-				end if;
-			elsif nRES='0' then
-				VCOUNT<=0;
-				HCOUNT<=0;
-        end if;
-    end process count_p;
+	SIGNAL rgb, squareCol : STD_LOGIC_VECTOR(2 DOWNTO 0) := "000";
+	--signal squareCol:std_logic_vector(2 downto 0):="000";
 
-    HSYNC_p:process(CLK, nRES) --SYNCS HSYNC at the correct timing
-    begin
-        if(nRES='1') then
-				if rising_edge(CLK) then
-					if (HCOUNT>275)and(HCOUNT<300)then
-						 HSYNC<='0';
-					else 
-						 HSYNC<='1';
-					end if;
-				end if;
-        elsif nRES='0' then
-				HSYNC<='0';
-			end if;
-    end process HSYNC_p;
+BEGIN
+	R <= rgb(0);
+	G <= rgb(1);
+	B <= rgb(2);
+	squareCol <= "100" WHEN xDir & yDir = "00" ELSE
+		"010" WHEN xDir & yDir = "01" ELSE
+		"001" WHEN xDir & yDir = "10" ELSE
+		"011" WHEN xDir & yDir = "11";
 
-    VSYNC_p:process(CLK, nRES) --SYNCS VSYNC at the correct timing
-    begin
-        if(nRES='1') then
-				if rising_edge(CLK) then
-					if(VCOUNT>600)and(VCOUNT<604)then
-						 VSYNC<='0';
-						 frame<='0';
-					else 
-						 VSYNC<='1';
-						 frame<='1';
-					end if;
-				end if;
-        elsif nRES='0' then
-				VSYNC<='0';
-				frame<='0';
-		  end if;
-    end process VSYNC_p;
-    
-    color_p:process(CLK, nRES) --outputs the proper colors of the object and background
-	variable xCirc : integer range -120 to 120;
-	variable yCirc : integer range -120 to 120;
-    begin
-    if (nRES='1') then
-	 if rising_edge(CLK) then
-        if(HCOUNT>0) and (HCOUNT<268) and (VCOUNT>0) and (VCOUNT<601)then --if in pixel send timeframe
-            if(HCOUNT>x)and(HCOUNT<x+40)and(VCOUNT>y)and(VCOUNT<y+120) then -- if current position is where object should be
-                if(obj='1')then --if the desired object is a circle
-                        xCirc:=(HCOUNT-x)*3-60;
-                        yCirc:=(VCOUNT-y)-60;
-                        if(xCirc*xCirc+yCirc*yCirc<=3601)then
-                            rgb<=squareCol;
-                        else
-                            rgb<="111";
-                        end if;			
-                else  -- if the desired object is a square
-                    rgb<=squareCol;
-                end if;
-		    else -- if current position isn't where a object should be
-                rgb<="111";
-            end if;
-		else            -- if current position isn't in the pixel send timeframe
-			rgb<="000";
-		end if;
-		end if;
-    elsif nRES='0' then
-		rgb<="000";
-	 end if;
-    end process color_p;
-	 
-	 dir_p:process(frame, nRES)
-    begin
-	 if(nRES='1') then
-		if(frame='1') and frame'event
-			then
-				beep<='0';
-				if(x>=266-40) then
-                xDir<='0';
-					 beep<='1';
-            elsif(x<=1) then
-                xDir<='1';
-					 beep<='1';
-            end if;
+	count_p : PROCESS (CLK, nRES) --counts and automatically resets the X and Y pixels
+	BEGIN
+		IF (nRES = '1') THEN
+			IF rising_edge(CLK) THEN
+				IF (HCOUNT /= 341) THEN
+					HCOUNT <= HCOUNT + 1;
+				ELSE
+					HCOUNT <= 0;
+					IF (VCOUNT /= 625) THEN
+						VCOUNT <= VCOUNT + 1;
+					ELSE
+						VCOUNT <= 0;
+					END IF;
+				END IF;
+			END IF;
+		ELSIF nRES = '0' THEN
+			VCOUNT <= 0;
+			HCOUNT <= 0;
+		END IF;
+	END PROCESS count_p;
 
-            if(y>=600-120) then
-                yDir<='0';
-					 beep<='1';
-            elsif (y<=1) then
-                yDir<='1';
-					 beep<='1';
-            end if; 
-		end if;
-		elsif nRES='0' then
-			xDir<=initXdir;
-			yDir<=initYdir;
-			beep<='0';
-		end if;
-    end process dir_p;
+	HSYNC_p : PROCESS (CLK, nRES) --SYNCS HSYNC at the correct timing
+	BEGIN
+		IF (nRES = '1') THEN
+			IF rising_edge(CLK) THEN
+				IF (HCOUNT > 275) AND (HCOUNT < 300) THEN
+					HSYNC <= '0';
+				ELSE
+					HSYNC <= '1';
+				END IF;
+			END IF;
+		ELSIF nRES = '0' THEN
+			HSYNC <= '0';
+		END IF;
+	END PROCESS HSYNC_p;
 
-    run_p:process(frame,nRES)
-    begin
-		if(nRES='1') then
-			if(frame='1') and frame'event then
-				 if(xDir='1') then
-					x<=x+1;
-				else 
-					x<=x-1;
-				end if;
+	VSYNC_p : PROCESS (CLK, nRES) --SYNCS VSYNC at the correct timing
+	BEGIN
+		IF (nRES = '1') THEN
+			IF rising_edge(CLK) THEN
+				IF (VCOUNT > 600) AND (VCOUNT < 604) THEN
+					VSYNC <= '0';
+					frame <= '0';
+				ELSE
+					VSYNC <= '1';
+					frame <= '1';
+				END IF;
+			END IF;
+		ELSIF nRES = '0' THEN
+			VSYNC <= '0';
+			frame <= '0';
+		END IF;
+	END PROCESS VSYNC_p;
 
-				 if(yDir='1') then
-					y<=y+1;
-				 else
-					y<=y-1;
-				end if;
-			end if;
-		elsif nRES='0' then
-			x<=initX;
-			y<=initY;
-		end if;
-	end process run_p;
-end architecture basicVGA_arch;
+	color_p : PROCESS (CLK, nRES) --outputs the proper colors of the object and background
+		VARIABLE xCirc : INTEGER RANGE -120 TO 120;
+		VARIABLE yCirc : INTEGER RANGE -120 TO 120;
+	BEGIN
+		IF (nRES = '1') THEN
+			IF rising_edge(CLK) THEN
+				IF (HCOUNT > 0) AND (HCOUNT < 268) AND (VCOUNT > 0) AND (VCOUNT < 601) THEN --if in pixel send timeframe
+					IF (HCOUNT > x) AND (HCOUNT < x + 40) AND (VCOUNT > y) AND (VCOUNT < y + 120) THEN -- if current position is where object should be
+						IF (obj = '1') THEN --if the desired object is a circle
+							xCirc := (HCOUNT - x) * 3 - 60;
+							yCirc := (VCOUNT - y) - 60;
+							IF (xCirc * xCirc + yCirc * yCirc <= 3601) THEN
+								rgb <= squareCol;
+							ELSE
+								rgb <= "111";
+							END IF;
+						ELSE -- if the desired object is a square
+							rgb <= squareCol;
+						END IF;
+					ELSE -- if current position isn't where a object should be
+						rgb <= "111";
+					END IF;
+				ELSE -- if current position isn't in the pixel send timeframe
+					rgb <= "000";
+				END IF;
+			END IF;
+		ELSIF nRES = '0' THEN
+			rgb <= "000";
+		END IF;
+	END PROCESS color_p;
+
+	dir_p : PROCESS (frame, nRES)
+	BEGIN
+		IF (nRES = '1') THEN
+			IF (frame = '1') AND frame'event
+				THEN
+				beep <= '0';
+				IF (x >= 266 - 40) THEN
+					xDir <= '0';
+					beep <= '1';
+				ELSIF (x <= 1) THEN
+					xDir <= '1';
+					beep <= '1';
+				END IF;
+
+				IF (y >= 600 - 120) THEN
+					yDir <= '0';
+					beep <= '1';
+				ELSIF (y <= 1) THEN
+					yDir <= '1';
+					beep <= '1';
+				END IF;
+			END IF;
+		ELSIF nRES = '0' THEN
+			xDir <= initXdir;
+			yDir <= initYdir;
+			beep <= '0';
+		END IF;
+	END PROCESS dir_p;
+
+	run_p : PROCESS (frame, nRES)
+	BEGIN
+		IF (nRES = '1') THEN
+			IF (frame = '1') AND frame'event THEN
+				IF (xDir = '1') THEN
+					x <= x + 1;
+				ELSE
+					x <= x - 1;
+				END IF;
+
+				IF (yDir = '1') THEN
+					y <= y + 1;
+				ELSE
+					y <= y - 1;
+				END IF;
+			END IF;
+		ELSIF nRES = '0' THEN
+			x <= initX;
+			y <= initY;
+		END IF;
+	END PROCESS run_p;
+END ARCHITECTURE basicVGA_arch;
